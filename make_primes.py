aff6126dba648aea369c05c6cd6ad65988089ea8
SUhonzz <uh5864@mci4me.at>
2023-11-21 11:57:44 +0100

    Added x,y

--- make_primes.py
+++ make_primes.py
@@ -1,5 +1,7 @@
 # Comments
-with open("data/primes.txt","w") as fo:
+x=0
+y=2000
+with open("Git/test_git_shell-master/test_git_shell/data/primes.txt","w") as fo:
     for n in range (x,y):
         if all(n%i!=0 for i in range (2,n)):
             a=[]
